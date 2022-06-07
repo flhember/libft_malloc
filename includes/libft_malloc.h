4ec28984f4a561e6136cfb42ecc3ffb654affc54
Florian HEMBERT <flhember@e1r6p2.42.fr>
2022-06-07 17:11:16 +0200

    Add struct for header of malloc

--- includes/libft_malloc.h
+++ includes/libft_malloc.h
@@ -1,8 +1,22 @@
 #ifndef LIBFT_MALLOC
 # define LIBFT_MALLOC
 
-#include <stddef.h>
 
+//Include
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+//Stuct
+struct block_meta {
+  size_t size;
+  struct block_meta *next;
+  int free;
+};
+
+//Proto
 void	*ft_malloc(size_t size);
+
 
 #endif
